de04d04bd2974c3f3527c6bad4e2755db4487c18
enriqueMontano <montanoenrique@outlook.com>
2020-06-10 23:48:22 +0200

    Add store & films reducers

---
+++ src/reducers/film.reducers.js
@@ -0,0 +1,7 @@
+const films = (state = [], action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
+export default films
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux"
+import films from "./film.reducers"
+
+export default combineReducers({
+    films,
+})
