f5dab356d4188b6bf9373ef5ab14453ab0cbea83
enriqueMontano <montanoenrique@outlook.com>
2020-06-13 21:53:21 +0200

    Create form to add films with genre field

--- src/containers/AddFilm.js
+++ src/containers/AddFilm.js
@@ -3,24 +3,54 @@ import { connect } from "react-redux"
 import { addFilm } from "../actions/film.actions"
 
 const AddFilm = ({ addFilm }) => {
-    const [value, setValue] = useState('')
+    const [title, setTitle] = useState('')
+    const [genre, setGenre] = useState('')
+    const [genres, setGenres] = useState([])
 
-    const handleOnChange = (e) => {
-        setValue(e.target.value)
+    const handleKeyPress = (e) => {
+        if (e.key === "Enter" && genre) {
+            setGenre('')
+            setGenres(genres.concat(genre))
+        }
     }
 
-    const handleAdd = () => {
-        if (value) {
-            setValue('')
-            addFilm(value)
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (title && genres.length) {
+            setTitle('')
+            setGenres([])
+            addFilm(title, genres)
         }
     }
 
     return (
-        <>
-            <input type="text" onChange={handleOnChange} value={value} placeholder="Write a movie you like..." />
-            <button onClick={handleAdd}>Add</button>
-        </>
+        <form onSubmit={handleSubmit} onKeyPress={e => e.key === "Enter" && e.preventDefault()}>
+            <label>
+                Title:
+                <input
+                    type="text"
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+                    placeholder="Write a movie you like!"
+                />
+            </label>
+            <label>
+                Genre:
+                <input
+                    type="text"
+                    value={genre}
+                    onChange={e => setGenre(e.target.value)}
+                    onKeyPress={handleKeyPress}
+                    placeholder="What genre does it belong to?"
+                />
+            </label>
+            <input type="submit" value="Submit" />
+            <div>
+                <ul>
+                    {genres != null && genres.map((genre, idx) => <li key={idx}>{genre}</li>)}
+                </ul>
+            </div>
+        </form>
     )
 }
 
