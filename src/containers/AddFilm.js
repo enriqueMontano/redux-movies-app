153b620104bbdb009086794c6aff4fdd000009d9
enriqueMontano <montanoenrique@outlook.com>
2020-06-13 15:47:52 +0200

    Improve AddFilm container with Hooks

--- src/containers/AddFilm.js
+++ src/containers/AddFilm.js
@@ -1,25 +1,27 @@
-import React from "react"
+import React, { useState } from "react"
 import { connect } from "react-redux"
 import { addFilm } from "../actions/film.actions"
 
-const AddFilm = ({ dispatch }) => {
+const AddFilm = ({ addFilm }) => {
+    const [value, setValue] = useState('')
 
-    const handleInputChange = (e) => {
-        // console.log(e.target.value)
+    const handleOnChange = (e) => {
+        setValue(e.target.value)
     }
 
-    const handleInputKeyPress = e => {
-        if (e.key === 'Enter' && e.target.value !== "") {
-            dispatch(addFilm(e.target.value))
-            document.querySelector("input").value = ""
+    const handleAdd = () => {
+        if (value) {
+            setValue('')
+            addFilm(value)
         }
     }
 
     return (
-        <React.Fragment>
-            <input type="text" onChange={handleInputChange} onKeyPress={handleInputKeyPress} />
-        </React.Fragment>
+        <>
+            <input type="text" onChange={handleOnChange} value={value} placeholder="Write a movie you like..." />
+            <button onClick={handleAdd}>Add</button>
+        </>
     )
 }
 
-export default connect()(AddFilm)
+export default connect(null, { addFilm })(AddFilm)
